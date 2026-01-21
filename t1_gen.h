6e7ed79b2990cce0b04d425eb80c4bbcd22afccb
xuwg <10461459@qq.com>
2026-01-21 17:17:45 +0800

    Add header file t1_gen.h for component library

---
+++ t1_gen.h
@@ -0,0 +1,84 @@
+/**
+ * @file t1_gen.h
+ */
+
+#ifndef T1_GEN_H
+#define T1_GEN_H
+
+#ifndef UI_SUBJECT_STRING_LENGTH
+#define UI_SUBJECT_STRING_LENGTH 256
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/*-------------------
+ * Permanent screens
+ *------------------*/
+
+/*----------------
+ * Global styles
+ *----------------*/
+
+/*----------------
+ * Fonts
+ *----------------*/
+
+/*----------------
+ * Images
+ *----------------*/
+
+/*----------------
+ * Subjects
+ *----------------*/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/*----------------
+ * Event Callbacks
+ *----------------*/
+
+/**
+ * Initialize the component library
+ */
+
+void t1_init_gen(const char * asset_path);
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   POST INCLUDES
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*T1_GEN_H*/
