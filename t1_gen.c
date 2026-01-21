5bbd09b4d21c740a77a38f6c14802a83bb5cba57
xuwg <10461459@qq.com>
2026-01-21 17:18:08 +0800

    Add initial implementation of t1_gen.c

---
+++ t1_gen.c
@@ -0,0 +1,118 @@
+/**
+ * @file t1_gen.c
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "t1_gen.h"
+
+#if LV_USE_XML
+#endif /* LV_USE_XML */
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/*----------------
+ * Translations
+ *----------------*/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/*--------------------
+ *  Permanent screens
+ *-------------------*/
+
+/*----------------
+ * Global styles
+ *----------------*/
+
+/*----------------
+ * Fonts
+ *----------------*/
+
+/*----------------
+ * Images
+ *----------------*/
+
+/*----------------
+ * Subjects
+ *----------------*/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+void t1_init_gen(const char * asset_path)
+{
+    char buf[256];
+
+    /*----------------
+     * Global styles
+     *----------------*/
+
+    /*----------------
+     * Fonts
+     *----------------*/
+
+
+    /*----------------
+     * Images
+     *----------------*/
+    /*----------------
+     * Subjects
+     *----------------*/
+    /*----------------
+     * Translations
+     *----------------*/
+
+#if LV_USE_XML
+    /* Register widgets */
+
+    /* Register fonts */
+
+    /* Register subjects */
+
+    /* Register callbacks */
+#endif
+
+    /* Register all the global assets so that they won't be created again when globals.xml is parsed.
+     * While running in the editor skip this step to update the preview when the XML changes */
+#if LV_USE_XML && !defined(LV_EDITOR_PREVIEW)
+    /* Register images */
+#endif
+
+#if LV_USE_XML == 0
+    /*--------------------
+     *  Permanent screens
+     *-------------------*/
+    /* If XML is enabled it's assumed that the permanent screens are created
+     * manaully from XML using lv_xml_create() */
+#endif
+}
+
+/* Callbacks */
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
